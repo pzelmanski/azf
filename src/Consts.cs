07078f065af268248bb4e14c50e70d1bcf3f32d4
pzelmanski <pzelmanski1@gmail.com>
2025-08-05 00:50:53 +0200

    comment

--- src/Consts.cs
+++ src/Consts.cs
@@ -1,6 +1,6 @@
 public static class CONSTS
 {
-    // This file could be configured in startup to read values from appsettings
+    // This file could be configured in startup to read values from appsettings (lets just assume it is)
     public const string _connectionString = "Data Source=mydatabase2.db";
     public const int JokesToInsertCount = 5;
     public const int MaxJokeLength = 200;
